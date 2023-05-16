c2a0754d8ce3e29612fd6b7ed65037203a0ce75a
Nandajit01 <ashru.dib@hotmail.com>
2023-05-16 00:13:46 -0400

    initial commit

---
+++ StudentsApp/Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+[ApiController]
+[Route("[controller]")]
+public class StudentController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult RetrieveStudents()
+    {
+        var students = new List<Student>();
+
+        // Create the first student
+        var alice = new Student
+        {
+            Name = "Alice",
+            Age = 20,
+            Hobbies = new List<string> { "reading", "swimming", "coding" }
+        };
+        students.Add(alice);
+
+        // Create the second student
+        var bob = new Student
+        {
+            Name = "Bob",
+            Age = 22,
+            Hobbies = new List<string> { "painting", "dancing", "singing" }
+        };
+        students.Add(bob);
+
+        var responseObj = new
+        {
+            students = students
+        };
+
+        return Ok(responseObj);
+    }
+}
+
---
+++ StudentsApp/Model/Student.cs
@@ -0,0 +1,8 @@
+﻿public class Student
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public List<string> Hobbies { get; set; }
+}
+
+
